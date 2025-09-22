fb09f5c725706d3970ff02ff762b985576f5a7a8
shereennew <shereeeennew@gmail.com>
2025-09-22 14:12:01 +0800

    added comment

--- transaction_history.py
+++ transaction_history.py
@@ -3,7 +3,7 @@ from datetime import datetime
 HISTORY_FILE = "transactions.txt"  # store all history here
 
 def add_transaction(kind, amount, balance):
-    """Save a transaction record with datetime into a file"""
+    #Save a transaction record with datetime into a file
     time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     record = f"{time_now} | {kind.upper()}: RM{amount:.2f}, Balance: RM{balance:.2f}"
     with open(HISTORY_FILE, "a") as f:   # append mode
